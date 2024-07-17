8024101c5cace7d5fb8052d90f84df08291ad438
vivek3214-coder <157635829+vivek3214-coder@users.noreply.github.com>
2024-07-17 02:44:21 +0000

    add

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-print("welcome to the caluclator")
-bill = float(input("what was your total bill $?\n"))
-tip = int(input("what percentage tip do you want to give?\n"))
-people = int(input("how many people are splitting the bill?\n"))
-total_with_tip = int(tip/100 * bill + bill)
-print(total_with_tip)
-if total_with_tip > 50:
-  print("That's a good tip Thank you!!")
-     
+print("Welcome to the rollercoaster")
+height = int(input("What is your height in cm?\n"))
+if height >= 120:
+  print("you can ride the rollercoaster")
+  age = int(input("What is your age?\n"))
+  if age <= 12:
+    print("please pay $5")
+  elif age <=18:
+    print("please pay $12")
+  else:
+    print("please pay $17")
+else:
+  print("Sorry you cannot ride rollercoaster")
+  
